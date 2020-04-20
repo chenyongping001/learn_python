7bfdcb98c7b8a80ceff0baabc07c7ddbd75d6cdc
cyp <chenyongping001@qq.com>
2020-04-20 22:56:48 +0800

    Initial commit

---
+++ 2_string.py
@@ -0,0 +1,23 @@
+name = "ada Lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello," + full_name + "!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Language:\nPython\nC\njava\nJavaScript")
+print("Language:\n\tPython\n\tC\n\tjava\n\tJavaScript")
+
+favorite_language = " python "
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+print(favorite_language)
