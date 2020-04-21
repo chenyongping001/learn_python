a29623d0aa9056834e80b853e02488b67a797c5f
cyp <chenyongping001@qq.com>
2020-04-21 23:30:19 +0800

    if

---
+++ 5_if_voting.py
@@ -0,0 +1,8 @@
+age = 17
+if age >= 18:
+    print("you are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+else:
+    print("Sorry,you are tool young to vote.")
+    print("Please register to vote as soon as yo turn 18!")
