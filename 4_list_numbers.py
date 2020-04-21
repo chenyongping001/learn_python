1e95c1265e359a2c89428795bcadd0909ec3ad1b
chen_workpc <chenyongping0001@qq.com>
2020-04-21 16:25:40 +0800

    3_list_cars.py

---
+++ 4_list_numbers.py
@@ -0,0 +1,10 @@
+from typing import List, Any
+
+for value in range(1, 5):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
