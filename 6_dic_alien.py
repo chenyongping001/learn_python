160dfc95f603859fce0d07b0aaf16cabdea4a488
chen_miBook <chenyongping001@qq.com>
2020-04-22 17:04:35 +0800

    6_dic

--- 6_dic_alien.py
+++ 6_dic_alien.py
@@ -17,3 +17,21 @@ print(alien_0)
 print("The alien is " + alien_0['color'] + ".")
 alien_0['color'] = 'yellow'
 print("The alien is " + alien_0['color'] + ".")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original X-position:" + str(alien_0['x_position']))
+# 向右移动外星人
+# 据外星人当前速度决定将其移动多远
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_0['x_position'] += x_increment
+print("New x-position:" + str(alien_0['x_position']))
+
+alien_0 = {'color': "green", 'points': 5}
+print(alien_0)
+del alien_0['points']
+print(alien_0)
---
+++ 6_dic_user.py
@@ -0,0 +1,8 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi'
+}
+for k, v in user_0.items():
+    print("key :" + k)
+    print("value :" + v)
