c883c160025208f2a90b9056c54d1dc5c1a22b71
cyp <chenyongping001@qq.com>
2020-04-24 06:30:11 +0800

    class _1

--- 4_list_squares.py
+++ 4_list_squares.py
@@ -11,6 +11,6 @@ print(sum(digits))
 squares = [value**2 for value in range(1,11)]
 print(squares)
 
-numbers = list(range(1,1000001))
-for value in numbers:
-    print(value)
+# numbers = list(range(1,1000001))
+# for value in numbers:
+#     print(value)
