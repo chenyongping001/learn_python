d70419aeda109857bbf19441883ba48a3550f5df
chen_workpc <chenyongping0001@qq.com>
2020-04-24 16:57:07 +0800

    10_file_except--1

---
+++ 10_file_reader.py
@@ -0,0 +1,11 @@
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    contents = file_object.read()
+    print(contents)
+    for line in file_object:
+        print(line.rstrip())
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
